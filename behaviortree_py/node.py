b2be62edd7ac3964993fdce851f1ec5f6233bf3c
Tomohiro Oku <tomohiro.oku@gmail.com>
2025-06-07 10:40:24 +0900

    sample 02

--- behaviortree_py/node.py
+++ behaviortree_py/node.py
@@ -57,5 +57,42 @@ class SequenceNode(ControlNode):
         if s == NodeStatus.SUCCESS and self._index < len(self.children) - 1:
             self._index += 1
             return NodeStatus.RUNNING
-        else:
-            return s
+        return s
+
+
+class FallbackNode(ControlNode):
+    def __init__(self, name, *children):
+        super().__init__(name, *children)
+        self._index = 0
+
+    def tick(self):
+        s = self.children[self._index].tick()
+        if s == NodeStatus.FAILURE and self._index < len(self.children) - 1:
+            self._index += 1
+            return NodeStatus.RUNNING
+        return s
+
+
+class Inverter(DecoratorNode):
+    def tick(self):
+        match self.child.tick():
+            case NodeStatus.SUCCESS:
+                return NodeStatus.FAILURE
+            case NodeStatus.FAILURE:
+                return NodeStatus.SUCCESS
+            case s:
+                return s
+
+
+class RetryUntilSuccessful(DecoratorNode):
+    def __init__(self, name, child, /, num_attempts: int):
+        super().__init__(name, child)
+        self.num_attempts = num_attempts
+        self._attempt = 0
+
+    def tick(self):
+        self._attempt += 1
+        s = self.child.tick()
+        if s == NodeStatus.FAILURE and self._attempt < self.num_attempts:
+            return NodeStatus.RUNNING
+        return s
---
+++ behaviortree_py/sample2.py
@@ -0,0 +1,62 @@
+from random import random
+
+from node import (
+    ActionNode,
+    NodeStatus,
+)
+
+
+class IsDoorOpen(ActionNode):
+    def tick(self):
+        if random() > 0.5:
+            print("Door is open")
+            return NodeStatus.SUCCESS
+        print("Door is closed")
+        return NodeStatus.FAILURE
+
+
+class OpenDoor(ActionNode):
+    def tick(self):
+        if random() > 0.8:
+            print("The door got opened")
+            return NodeStatus.SUCCESS
+        print("The door won't open")
+        return NodeStatus.FAILURE
+
+
+class EnterRoom(ActionNode):
+    def tick(self):
+        print("Enter the room")
+        return NodeStatus.SUCCESS
+
+
+class CloseDoor(ActionNode):
+    def tick(self):
+        print("Close the door")
+        return NodeStatus.SUCCESS
+
+
+if __name__ == "__main__":
+    from node import (
+        FallbackNode,
+        Inverter,
+        RetryUntilSuccessful,
+        SequenceNode,
+    )
+
+    tree = SequenceNode(
+        None,
+        FallbackNode(
+            None,
+            IsDoorOpen(),
+            RetryUntilSuccessful(None, OpenDoor(), num_attempts=5),
+        ),
+        EnterRoom(),
+        CloseDoor(),
+    )
+
+    s = NodeStatus.RUNNING
+    while s == NodeStatus.RUNNING:
+        print("tick once")
+        s = tree.tick()
+    print("The action finished", s.name)
