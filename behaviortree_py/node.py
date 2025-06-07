5b187788e50f0307465501e967049d738cd3d826
Tomohiro Oku <tomohiro.oku@gmail.com>
2025-06-07 09:42:04 +0900

    sample 01

--- behaviortree_py/node.py
+++ behaviortree_py/node.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from enum import Enum, auto
 
 
@@ -5,3 +6,56 @@ class NodeStatus(Enum):
     SUCCESS = auto()
     FAILURE = auto()
     RUNNING = auto()
+
+
+class TreeNode(ABC):
+    def __init__(self, name: str | None = None):
+        self.name = name or self.__class__.__name__.removesuffix("Node")
+
+    @abstractmethod
+    def tick(self) -> NodeStatus:
+        pass
+
+
+class LeafNode(TreeNode):
+    def tick(self):
+        pass
+
+
+class ActionNode(LeafNode):
+    def tick(self):
+        pass
+
+
+class DecoratorNode(TreeNode):
+    def __init__(self, name, child: TreeNode):
+        super().__init__(name)
+        self.child = child
+
+    pass
+
+
+class ControlNode(TreeNode):
+    def __init__(self, name, *children: TreeNode):
+        super().__init__(name)
+        self.children = children
+
+    pass
+
+
+class ConditionNode(LeafNode):
+    pass
+
+
+class SequenceNode(ControlNode):
+    def __init__(self, name, *children):
+        super().__init__(name, *children)
+        self._index = 0
+
+    def tick(self):
+        s = self.children[self._index].tick()
+        if s == NodeStatus.SUCCESS and self._index < len(self.children) - 1:
+            self._index += 1
+            return NodeStatus.RUNNING
+        else:
+            return s
---
+++ behaviortree_py/sample1.py
@@ -0,0 +1,28 @@
+from node import ActionNode, NodeStatus, SequenceNode
+
+
+class OpenFridge(ActionNode):
+    def tick(self):
+        print("Open fridge")
+        return NodeStatus.SUCCESS
+
+
+class GrabBeer(ActionNode):
+    def tick(self):
+        print("Grab beer")
+        return NodeStatus.SUCCESS
+
+
+class CloseFridge(ActionNode):
+    def tick(self):
+        print("Close fridge")
+        return NodeStatus.SUCCESS
+
+
+if __name__ == "__main__":
+    tree = SequenceNode(None, OpenFridge(), GrabBeer(), CloseFridge())
+    s = NodeStatus.RUNNING
+    while s == NodeStatus.RUNNING:
+        print("tick once")
+        s = tree.tick()
+    print("tree finished", s.name)
