031319ead0e9f2260368c87bc8f09c1ccd6b06f8
Tomohiro Oku <tomohiro.oku@gmail.com>
2025-06-07 11:05:40 +0900

    remove unused

--- behaviortree_py/sample2.py
+++ behaviortree_py/sample2.py
@@ -39,7 +39,6 @@ class CloseDoor(ActionNode):
 if __name__ == "__main__":
     from node import (
         FallbackNode,
-        Inverter,
         RetryUntilSuccessful,
         SequenceNode,
     )
