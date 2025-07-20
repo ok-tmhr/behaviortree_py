a29bd6f24b4d6621e2807756a807dfedd106b445
Tomohiro Oku <tomohiro.oku@gmail.com>
2025-07-20 17:33:58 +0900

    fix ticking logic

--- src/behaviortree_py/library/control_node.py
+++ src/behaviortree_py/library/control_node.py
@@ -6,17 +6,17 @@ __all__ = "Sequence", "Fallback"
 
 class Sequence(ControlNode):
     def tick(self) -> NodeStatus:
-        s = self.child[self._index].tick()
-        if s == NodeStatus.SUCCESS and self._index < len(self.child) - 1:
+        while (
+            s := self.child[self._index].tick()
+        ) == NodeStatus.SUCCESS and self._index < len(self.child) - 1:
             self._index += 1
-            return NodeStatus.RUNNING
         return s
 
 
 class Fallback(ControlNode):
     def tick(self) -> NodeStatus:
-        s = self.child[self._index].tick()
-        if s == NodeStatus.FAILURE and self._index < len(self.child) - 1:
+        if (
+            s := self.child[self._index].tick()
+        ) == NodeStatus.FAILURE and self._index < len(self.child) - 1:
             self._index += 1
-            return NodeStatus.RUNNING
         return s
